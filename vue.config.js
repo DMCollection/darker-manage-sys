da5d3420cfb79bf4a58ffb2d5c93fb37417b5610
plentymore <plentymorea@gmail.com>
2018-07-25 00:09:36 +0800

    be ready to publish*3

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,14 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
-  chainWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      config
-          .plugin('uglify')
-          .tap(([options]) => {
-            // 去除 console.log
-            return [Object.assign(options, {
-              uglifyOptions: {
-                compress: {
-                  drop_console: true,
-                  pure_funcs: ['console.log']
-                }
-              }
-            })]
-          })
-    }
+  configureWebpack: {
+    plugins: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
+    ]
   }
 };
